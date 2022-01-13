6671bb60f62cff4594fd2bb47ee92949cf168d08
crgargar <cgargar542@g.educaand.es>
2022-01-13 13:00:59 +0100

    cgg- actualizacion clase

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
-namespace GetStartedDebugging
+class ArrayExample
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        //define un array de caracteres llamados letters
+        //se reserva memoria para los 10 elementos de tipo char
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+        // blucle for que va mostrando las letras cada vez que da la vuelta.
+        for (int i = 0; i < letters.Length; i++) //cuando entre aqui se suma uno a lo que ya tenemos
         {
-            Console.WriteLine("Hello World!");
+            name += letters[i]; //punto de interrupcion
+            a[i] = i + 1;
+            SendMessage(name, a[i]); //es la variable name mas la letra que se le a incluido
         }
+        Console.ReadKey();
+    }
+    static void SendMessage(string name, int msg)
+    {
+        //muestra por pantalla la palabra hello con la continuacion de la letra que va cogiendo en cada vuelta que da
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
     }
 }
